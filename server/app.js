32d57d4f14bed4bb8ec2d8cee480e51be5851643
KaustubhD <kaustubhd287@gmail.com>
2019-01-08 00:47:38 +0530

    Removing Graphiql

--- server/app.js
+++ server/app.js
@@ -6,11 +6,6 @@ const schema = require('./config/schema')
 
 const app = express()
 
-// mongoose.connect('mongodb://kay:papaya25@ds157320.mlab.com:57320/graphql-sample', {useNewUrlParser: true})
-// mongoose.connection.once('open', () => {
-//   console.log('Connected')
-// })
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -19,16 +14,12 @@ app.use(function(req, res, next) {
 
 app.post('/graphql', graphqlHTTP({
   schema,
-  graphiql: true
+  // graphiql: true
 }))
 app.get('/graphql', graphqlHTTP({
   schema,
-  graphiql: true
+  // graphiql: true
 }))
-
-// app.use('/', (req, res) => {
-//   res.send('Got it')
-// })
 
 app.listen(4000, () => {
   console.log('Listening')
